26fc4671a958a8f1e38d36c18a9cd741a03c133b
Harshit Katiyar <71125137+harshaster@users.noreply.github.com>
2022-07-24 22:56:37 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \"21f1004629@student.onlinedegree.iitm.ac.in\"\n\
+email = \"21f1006135@student.onlinedegree.iitm.ac.in\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
